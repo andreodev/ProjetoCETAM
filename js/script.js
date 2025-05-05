b26f34ad0f1eb14a14f09a0c4e838b7aa1619650
andreodev <developerandreo@gmail.com>
2025-05-05 06:01:46 -0300

    🔥 refactoring in html index and step-by-step creation in script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// passo a passo do aprendendo sons rederização de categorias 
+
+// /* nova estrutura do html
+//    <div class="container" id="card-categorias">
+//         <div class="row row-top" id="row-top"></div>
+//         <div class="row row-bottom" id="row-bottom"></div>
+//     </div>
+// */
+
+// //1. arrumar um objeto para os cards
+// //2. Fazer uma função para criar os cards
+// //3. Fazer uma função para renderizar os cards
+// //4. Deixe a primeira palavra em maiusculo pra evitar erros na renderização do card
